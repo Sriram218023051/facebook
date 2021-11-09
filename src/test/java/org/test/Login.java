39ff7946f0a38fa216abb8c47120e6001c7703d9
Sriram218023051 <sisriram28@gmail.com>
2021-11-09 11:27:28 +0530

    username functionality

--- src/test/java/org/test/Login.java
+++ src/test/java/org/test/Login.java
@@ -1,5 +1,11 @@
 package org.test;
 
 public class Login {
-
+	
+	public static void userName() {
+		System.out.println("Username entered");
+	}
+	public static void main(String[] args) {
+		userName();
+	}
 }
